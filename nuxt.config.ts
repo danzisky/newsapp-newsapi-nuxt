9f0b8bd377fb9c8fdc922407dd3b8a7f6b5289a3
Daniel <dannie005@gmail.com>
2024-03-08 00:44:02 +0200

    chore: first commit - add base files and dependencies

---
+++ nuxt.config.ts
@@ -0,0 +1,10 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@vueuse/nuxt',
+    '@formkit/auto-animate/nuxt',
+    '@pinia/nuxt',
+  ]
+})
